ab78ba6e6905a4f1b05e9b0cc83679e8cf44c822
KhondakarAfridi <afridi.khondakar@gmail.com>
2023-12-16 13:55:04 +0600

    Homepage : wip

--- PetShop/Features/Home/Views/HomeView.swift
+++ PetShop/Features/Home/Views/HomeView.swift
@@ -24,6 +24,71 @@ struct HomeView: View {
                             .foregroundStyle(.orange)
                     }
                     .padding(.horizontal, 16)
+                    HStack{
+                        Spacer()
+                        Spacer()
+                            ZStack {
+                                Color("F8F8F8")
+                                Image(systemName: "arrow.left.arrow.right")
+                                    .font(.subheadline)
+                                    .fontWeight(.medium)
+                                    .padding(.vertical, 15)
+                            }
+                            .frame(width: 60)
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .onTapGesture {}
+                        Spacer()
+                        ZStack {
+                            true ? Color(.orange) : Color("F8F8F8")
+                            Text("Food")
+                                .font(.subheadline)
+                                .fontWeight(.medium)
+                                .foregroundColor(true ? Color(.white) : Color("898989") )
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .padding(.vertical, 15)
+                        }
+                        .frame(width: 70)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .onTapGesture {}
+                        Spacer()
+                        ZStack {
+                            false ? Color(.orange) : Color("F8F8F8")
+                            Text("Toys")
+                                .font(.subheadline)
+                                .fontWeight(.medium)
+                                .foregroundColor(false ? Color(.white) : Color("898989") )
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .padding(.vertical, 15)
+                        }
+                        .frame(width: 70)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .onTapGesture {}
+                        Spacer()
+                        ZStack {
+                            false ? Color(.orange) : Color("F8F8F8")
+                            Text("Accessories")
+                                .font(.subheadline)
+                                .fontWeight(.medium)
+                                .foregroundColor(false ? Color(.white) : Color("898989") )
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .padding(.vertical, 15)
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .onTapGesture {}
+                        Spacer()
+                        Spacer()
+                    }
+                    .padding(.bottom, 10)
+                    HStack(alignment: .bottom) {
+                        Text("Best Seller")
+                            .font(.title2)
+                        .fontWeight(.bold)
+                        Spacer()
+                        Text("View All")
+                            .font(.body)
+                            .foregroundStyle(.orange)
+                    }
+                    .padding(.horizontal, 16)
                     Spacer()
                 }
             }
